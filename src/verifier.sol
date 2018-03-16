fc69d0ea961c835d8487a7ccdeb641e291314426
alex <alex.tran234@gmail.com>
2018-03-17 07:16:50 +1100

    implemented a simple verifier by calling ecrecover

--- src/verifier.sol
+++ src/verifier.sol
@@ -1,4 +1,9 @@
 pragma solidity ^0.4.21;
 
 contract verifier {
+
+	function isSigned(address _addr, bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) external pure returns (bool) {
+		return ecrecover(msgHash, v, r, s) == _addr;
+	}
+
 }
--- src/verifier.t.sol
+++ src/verifier.t.sol
@@ -5,17 +5,9 @@ import "ds-test/test.sol";
 import "./verifier.sol";
 
 contract verifierTest is DSTest {
-    verifier verify;
+	verifier verify;
 
-    function setUp() public {
-        verify = new verifier();
-    }
-
-    function testFail_basic_sanity() public {
-        assertTrue(false);
-    }
-
-    function test_basic_sanity() public {
-        assertTrue(true);
-    }
+	function setUp() public {
+		verify = new verifier();
+	}
 }
