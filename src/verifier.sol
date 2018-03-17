1f97efa4407d22d14a627219f509912c842facf9
alex <alex.tran234@gmail.com>
2018-03-18 07:01:00 +1100

    add getSignAddress to verifier

--- src/verifier.sol
+++ src/verifier.sol
@@ -2,6 +2,10 @@ pragma solidity ^0.4.21;
 
 contract verifier {
 
+	function getSignAddress(bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) external pure returns (address) {
+		return ecrecover(msgHash, v, r, s);
+	}
+
 	function isSigned(address _addr, bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) external pure returns (bool) {
 		return ecrecover(msgHash, v, r, s) == _addr;
 	}
