96687a59170f3110e763fc84bb47ae54a579b83b
alex <alex.tran234@gmail.com>
2018-03-19 06:48:07 +1100

    implementing web3 signing

---
+++ testnet-web3.js
@@ -0,0 +1,21 @@
+const Web3 = require('web3')
+const provider = new Web3.providers.HttpProvider('http://localhost:8545')
+const web3 = new Web3(provider)
+
+
+getAccountsCallback = function(error, results) {
+	let addr = results[1]
+	let msg = 'Hello World'
+	let hexDataFixed = web3.utils.keccak256(msg)
+	console.log("addr = " + addr)
+	console.log("hexMsgFixed = " + hexDataFixed)
+
+	web3.eth.sign(hexDataFixed, addr, signCallback);
+}
+
+signCallback = function(error, results) {
+	console.log(results);
+}
+
+
+web3.eth.getAccounts(getAccountsCallback)
