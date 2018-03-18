5f31faf952d7e2d58b3d1740a36ad745eb539c6a
alex <alex.tran234@gmail.com>
2018-03-18 20:13:07 +1100

    update some comment about the use of prefix

--- src/verifier.sol
+++ src/verifier.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.4.21;
 contract verifier {
 
 	function getSignAddress(bytes32 msgHash, uint8 v, bytes32 r, bytes32 s) external pure returns (address) {
+		// this prefix is needed when signing via geth client
+		// https://github.com/ethereum/go-ethereum/issues/3731
 		bytes memory prefix = "\x19Ethereum Signed Message:\n32";
 		bytes32 prefixedHash = keccak256(prefix, msgHash);
 
