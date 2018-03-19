db13f94c2460961fac31809f88ac719ebb6048dd
alex <alex.tran234@gmail.com>
2018-03-20 06:55:20 +1100

    add contract interface

--- testnet-web3.js
+++ testnet-web3.js
@@ -3,6 +3,8 @@ const provider = new Web3.providers.HttpProvider('http://localhost:8545')
 const web3 = new Web3(provider)
 
 
+
+
 getAccountsCallback = function(error, results) {
 	let addr = results[1]
 	let msg = 'Hello World'
@@ -14,7 +16,15 @@ getAccountsCallback = function(error, results) {
 }
 
 signCallback = function(error, results) {
-	console.log(results);
+	console.log("signature = " + results)
+	// TODO: split results into r,s,v
+
+	var fs = require('fs');
+	var abiFile="./out/verifier.abi"
+	var parsedAbi= JSON.parse(fs.readFileSync(abiFile));
+	const verifier = new web3.eth.Contract(parsedAbi)
+	// TODO: call verifier to check
+
 }
 
 
