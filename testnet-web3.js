31394b87e644d32c776d65b78576176ce6751a2f
alex <alex.tran234@gmail.com>
2018-03-22 06:26:05 +1100

    a working script, to be refactored

--- testnet-web3.js
+++ testnet-web3.js
@@ -32,28 +32,46 @@ signCallback = function(error, results) {
 	// deploy the verifier contract
 	var fs = require('fs');
 	var abiFile="./out/verifier.abi"
+	var binFile="./out/verifier.bin"
 	var parsedAbi= JSON.parse(fs.readFileSync(abiFile));
-	const contractAddress='0x7cc98f5ca41692551a6d102a8d97471baa68fa11'
-	const verifier = new web3.eth.Contract(parsedAbi, 
-			contractAddress)
+	var byteCode = '0x'+fs.readFileSync(binFile)
+	console.log("byteCode = " + byteCode)
+	//const contractAddress='0x7cc98f5ca41692551a6d102a8d97471baa68fa11'
+	//const verifier = new web3.eth.Contract(parsedAbi, 
+	//		contractAddress)
+	//verifier.setProvider(provider)
+	
+	const verifier = new web3.eth.Contract(parsedAbi)
 	verifier.setProvider(provider)
-	
+	verifier.options.data='0x00'
 	const ethFromAddress='0xc216b37229100cd054ac9e08772f59bcb7fa4a13'
-	const signerAddress='0x4633C93EF85845AfFF73E606c9Daef4367fAc755'
-	verifier.methods
-		.isSigned(signerAddress, hexDataFixed, v, r, s)
-		.call({from: ethFromAddress})
-		.then(result => {
-			console.log("isSigned = " + result)
-		})
+		
+	verifier
+	.deploy({
+		data: byteCode
+	})
+	.send({
+		from: ethFromAddress,
+		gas: 4500000,
+		gasPrice: '18000000000' 
+	})
+	.on('error',console.log)
+	.then(contractInstance=> {
+		contractAddress = contractInstance.options.address
+		console.log("verifier deployed at address "+ contractAddress)
+		// call the isSigned method on the contract
+		const ethFromAddress='0xc216b37229100cd054ac9e08772f59bcb7fa4a13'
+		const signerAddress='0x4633C93EF85845AfFF73E606c9Daef4367fAc755'
+		contractInstance.methods
+			.isSigned(signerAddress, hexDataFixed, v, r, s)
+			.call({from: ethFromAddress})
+			.then(result => {
+				console.log("isSigned = " + result)
+			})
+	})
+
 	
-	// TODO: deplot returns null??
-//	verifier.deploy()
-//		.on('error', error => { console.log("Error :"+error) })
-//		.on('confirmation', confirmationNumber, receipt => { console.log("confirmationNumber "+ confirmationNumber) })
-//		.then(instance => {
-//			console.log("contract instance "+ instance)
-//		})
+
 }
 
 
