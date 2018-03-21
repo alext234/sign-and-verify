d621d6a63e788b140afa2ce49e95bf4743d321ed
alex <alex.tran234@gmail.com>
2018-03-22 06:42:52 +1100

    refactor a bit web3 code

--- testnet-web3.js
+++ testnet-web3.js
@@ -1,19 +1,33 @@
 const Web3 = require('web3')
 const provider = new Web3.providers.HttpProvider('http://localhost:8545')
 const web3 = new Web3(provider)
+const abiFile="./out/verifier.abi"
+const binFile="./out/verifier.bin"
 
+var signerAddress;
+var ethFromAddressToCallVerifier;
+var ethFromAddressToDeployVerifier;
 
+// message to be signed
 const msg = 'Hello World'
 const hexDataFixed = web3.utils.keccak256(msg)
 
 
 getAccountsCallback = function(error, results) {
-	const addr = results[1]
-	console.log("addr = " + addr)
+	
+	// expect to have at least 2 accounts
+	ethFromAddressToCallVerifier=results[0]
+	ethFromAddressToDeployVerifier=results[0]
+	signerAddress=results[1]
+	
+	console.log("signing the data:")
+	console.log("signerAddress= " + signerAddress)
 	console.log("hexMsgFixed = " + hexDataFixed)
 
-	web3.eth.sign(hexDataFixed, addr, signCallback);
+	web3.eth.sign(hexDataFixed, signerAddress, signCallback);
 }
+
+
 
 signCallback = function(error, results) {
 	console.log("signature = " + results)
@@ -28,51 +42,43 @@ signCallback = function(error, results) {
 	console.log('v = ' + v)
 	console.log('v_decimal = ' + v_decimal)
 	
+	// deploy the verifier contract and once deployed, call the isSigned method
+	deployVerifierContract( contractInstance => {
+		contractAddress = contractInstance.options.address
+		console.log("verifier deployed at address "+ contractAddress)
+		// call the isSigned method on the contract
+		contractInstance.methods
+			.isSigned(signerAddress, hexDataFixed, v, r, s)
+			.call({from: ethFromAddressToCallVerifier})
+			.then(result => {
+				console.log("isSigned = " + result)
+			})
+	});
 
-	// deploy the verifier contract
-	var fs = require('fs');
-	var abiFile="./out/verifier.abi"
-	var binFile="./out/verifier.bin"
-	var parsedAbi= JSON.parse(fs.readFileSync(abiFile));
-	var byteCode = '0x'+fs.readFileSync(binFile)
+}
+
+function deployVerifierContract(deployCallback) {
+	let fs = require('fs');
+	let parsedAbi= JSON.parse(fs.readFileSync(abiFile));
+	let byteCode = '0x'+fs.readFileSync(binFile)
 	console.log("byteCode = " + byteCode)
-	//const contractAddress='0x7cc98f5ca41692551a6d102a8d97471baa68fa11'
-	//const verifier = new web3.eth.Contract(parsedAbi, 
-	//		contractAddress)
-	//verifier.setProvider(provider)
 	
 	const verifier = new web3.eth.Contract(parsedAbi)
 	verifier.setProvider(provider)
-	verifier.options.data='0x00'
-	const ethFromAddress='0xc216b37229100cd054ac9e08772f59bcb7fa4a13'
 		
 	verifier
 	.deploy({
 		data: byteCode
 	})
 	.send({
-		from: ethFromAddress,
+		from: ethFromAddressToDeployVerifier,
 		gas: 4500000,
 		gasPrice: '18000000000' 
 	})
 	.on('error',console.log)
 	.then(contractInstance=> {
-		contractAddress = contractInstance.options.address
-		console.log("verifier deployed at address "+ contractAddress)
-		// call the isSigned method on the contract
-		const ethFromAddress='0xc216b37229100cd054ac9e08772f59bcb7fa4a13'
-		const signerAddress='0x4633C93EF85845AfFF73E606c9Daef4367fAc755'
-		contractInstance.methods
-			.isSigned(signerAddress, hexDataFixed, v, r, s)
-			.call({from: ethFromAddress})
-			.then(result => {
-				console.log("isSigned = " + result)
-			})
+		deployCallback(contractInstance)
 	})
-
-	
-
 }
-
 
 web3.eth.getAccounts(getAccountsCallback)
