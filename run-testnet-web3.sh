20aa9aef8f612b86884dd6c6d265ee36d27bea90
alex <alex.tran234@gmail.com>
2018-04-04 07:12:27 +1000

    for debugging travis run

--- run-testnet-web3.sh
+++ run-testnet-web3.sh
@@ -1,3 +1,9 @@
+set -x
+
+# first run to see the output
+node testnet-web3.js
+
+# second run to have some checking
 node testnet-web3.js 2>&1| grep "Unhandled rejection Error"  
 
 if [ $? == 0 ]; then
